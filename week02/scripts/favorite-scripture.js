d112a77c3912064da03be0b79096facba7d79470
Secret-Doughnut <dustinorangecook@gmail.com>
2025-11-04 15:54:00 -0700

    I might of finished the current version of the Favorite Scriptures List thing. MAYBE!

--- week02/scripts/favorite-scripture.js
+++ week02/scripts/favorite-scripture.js
@@ -1,23 +1,27 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
-
+const list = document.querySelector('ul');
 const deleteButton = document.createElement('button');
+const li = document.createElement('li');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
 
-li.append(deleteButton);
-list.append(li);
+
 
 
 button.addEventListener('click', function () {
-    if (input.value.trim() !== '') { 
-        const li = document.createElement('li');
-        li.textContent = input.value.trim();
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
         list.appendChild(li);
+        li.append(deleteButton);
+        list.append(li);
+        input.focus();
     }
-    else {
-        list.appendChild("Hello!")
-    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+    input.value = '';
+    input.focus();
 });
