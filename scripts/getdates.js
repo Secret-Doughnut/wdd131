7cea32f9316cdd99d487f895dfd41ea22ebee18a
Secret-Doughnut <dustinorangecook@gmail.com>
2025-11-22 17:43:43 -0700

    Fixed a small date info in JS.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@ const today = new Date();
 const hour = today.getHours();
 const minute = today.getMinutes();
 const day = today.getDate();
-const month = today.getMonth();
+const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
 lastModified.innerHTML = `Last modified on: ${month}/${day}/${year} ${hour}:${minute}`
